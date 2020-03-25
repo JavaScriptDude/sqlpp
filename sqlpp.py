b67019c76a92f14792947eb684782883bc78c694
JavaScriptDude <tim.c.quinn@gmail.com>
2020-03-25 15:29:09 -0400

    Adding proper exit codes and error handling

--- sqlpp.py
+++ sqlpp.py
@@ -15,27 +15,24 @@
 # Licence: https://opensource.org/licenses/MIT
 #########################################
 
-import argparse, sqlparse, re
+import argparse, sqlparse, re, sys, traceback
 
 parser = argparse.ArgumentParser(prog="sqlpp")
 parser.add_argument("--verbose", "-v", action='store_true')
+parser.add_argument("--stdin", "-s", action='store_true')
 parser.add_argument("--comma_first", default=False, action='store_true')
 
-parser.add_argument("file", type=argparse.FileType("r"), nargs="+")
+parser.add_argument("file", type=argparse.FileType("r"), nargs="*")
 
 args = parser.parse_args()
 
 def prepend(s, s2): return s2 + re.sub('\n', '\n'+s2, s)
 
-# Pretty print input files
-n=len(args.file)
-for i, file in enumerate(args.file):
-    sIn = file.read().replace('\n', '')
-    file.close()
+def pprint(sIn, n=-1):
     sOut = sqlparse.format(sIn, reindent=True, keyword_case='upper', use_space_around_operators=True, comma_first=args.comma_first)
     if args.verbose or n > 1: 
         print("File{0}:\n    {1}\n{2}\nFormatted SQL:\n{3}\n".format(
-             (' ' + str(i+1) if n > 1 else '')
+            (' ' + str(i+1) if n > 1 else '')
             ,file.name
             ,("\nOriginal SQL:\n{}\n".format(prepend(sIn, "    ")) 
                     if args.verbose else "")
@@ -43,3 +40,29 @@ for i, file in enumerate(args.file):
         ))
     else:
         print(sOut)
+try:
+    if args.stdin:
+        sb = []
+        for line in sys.stdin:
+            sb.append(line)
+
+        pprint('\n'.join(sb))
+
+    else:
+
+        n=len(args.file)
+        if n== 0:
+            parser.print_help()
+            print("One or more files required")
+        for i, file in enumerate(args.file):
+            sIn = file.read().replace('\n', '')
+            file.close()
+            pprint(sIn, n)
+    
+    sys.exit(0)
+
+except Exception as e:
+    exc_type, exc_value, exc_traceback = sys.exc_info()
+    sTB = '\n'.join(traceback.format_tb(exc_traceback))
+    print("Fatal exception: {}\n - msg: {}\n stack: {}".format(exc_type, exc_value, sTB))
+    sys.exit(1)
